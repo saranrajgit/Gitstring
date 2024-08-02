b181b8b3fc1b47679c0546f7209e863d79a84e7a
saranrajgit <SARANRAJ2801@GMAIL.COM>
2024-08-02 20:28:27 +0530

    gitstringcode

---
+++ src/org/string/Testconcat.java
@@ -0,0 +1,10 @@
+package org.string;
+
+public class Testconcat {
+public static void main(String[] args) {
+	String s  = "java";
+	String s1 = "java";
+	String c = s.concat(s1);
+	System.out.println(c);
+}
+}
